04df5531d2d2654ac2871bc5ca15ca29e8ecb8c1
alexa665 <a.23308051281205@cbtis128.edu.mx>
2024-09-24 20:48:51 -0600

    Add files via upload

---
+++ pr11.py
@@ -0,0 +1,9 @@
+print(" ")
+print("alexa guadalupe ramirez manzo")
+print(" ")
+print("de fahrenheit a celsius") #nombre del programa 
+print(" ")
+f=float(input("ingrese la temperatura"))#variable y linea para ingresar la temperatura
+print(" ")
+c= (5/9) * (f -32) #formula para que f se cambierta a c
+print ("la temperatura es",c) #inpresion de las temperaturas
